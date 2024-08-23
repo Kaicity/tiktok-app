8492e638bbf3633c37315fe60694ffb3f16f8789
Nguyễn Minh Thông <nguyenminhthongITMIX@gmail.com>
2024-08-24 01:03:52 +0700

    Build Header

--- src/components/Layout/DefaultLayout/index.js
+++ src/components/Layout/DefaultLayout/index.js
@@ -1,13 +1,17 @@
+import styles from "./DefaultLayout.module.scss";
+import classNames from "classnames/bind";
 import Header from "@/components/Layout/components/Header";
 import Sidebar from "./Sidebar";
 
+const cx = classNames.bind(styles);
+
 function DefaultLayout({ children }) {
   return (
-    <div className="">
+    <div className={cx("wrapper")}>
       <Header />
-      <div className="container">
+      <div className={cx("container")}>
         <Sidebar />
-        <div className="content">{children}</div>
+        <div className={cx("content")}>{children}</div>
       </div>
     </div>
   );
